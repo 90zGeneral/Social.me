78662ac7abf3fdebdaf9cc40e709aa36b12150ac
Roydon Jeffrey <brushdam14823@gmail.com>
2017-03-20 13:47:55 -0400

    Renamed the controller file to a more appropriate name based on features.

---
+++ Social.me/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  SignInVC.swift
+//  Social.me
+//
+//  Created by Roydon Jeffrey on 3/20/17.
+//  Copyright © 2017 Italyte. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
